8c24bba8f55a1679d02eb126d9d43d81ec925597
Evasc0 <145756607+Evasc0@users.noreply.github.com>
2025-02-20 13:41:10 +0000

    redo migration and model

--- SimpleRoutingViews/resources/views/itemShow.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('itemTemplate')
-
-@section('content')
-    <div class="item-details">
-        <h1>{{ $name }}</h1>
-        <p>{{ $description }}</p>
-        <a href="{{ route('items.edit', $id) }}" class="btn btn-primary">Edit Item</a>
-    </div>
-@endsection
--- SimpleRoutingViews/routes/web.php
+++ SimpleRoutingViews/routes/web.php
@@ -12,22 +12,3 @@ Route::get('/hello', function () {
 });
 
 Route::get('/greet', [GreetController::class, 'greet']);
-
-Route::get('posts', function(){
-    $res = Post::with('comments')->get();
-    dd($res->toArray());
-});
-
-Route::prefix('test')
-    ->middleware(CheckParam::class)
-    ->group(function () {
-    Route::get('/', function () {
-        return "This is the /test route.";
-    });
-
-    Route::get('{value}', function ($value) {
-        return "Accepted: " . $value;
-    });
-});
-
-Route::resource('items', ItemsController::class);
